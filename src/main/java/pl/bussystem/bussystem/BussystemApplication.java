dd579d65cee1a0d9e871bd47deba7df8a628fd70
Paweł P <panczo12d@gmail.com>
2018-03-02 09:44:38 +0100

    init

---
+++ src/main/java/pl/bussystem/bussystem/BussystemApplication.java
@@ -0,0 +1,12 @@
+package pl.bussystem.bussystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BussystemApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/bussystem/bussystem/BussystemApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.bussystem.bussystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BussystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
