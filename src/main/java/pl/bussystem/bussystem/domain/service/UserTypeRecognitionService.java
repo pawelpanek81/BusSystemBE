c703db65ae1e88ae39f62c8cf40f9582a989e2e2
Grzegorz Chilczuk <chilczukg@gmail.com>
2018-03-17 10:42:18 +0100

    reconize user type

--- src/main/java/pl/bussystem/bussystem/domain/service/UserTypeRecognitionService.java
+++ src/main/java/pl/bussystem/bussystem/domain/service/UserTypeRecognitionService.java
@@ -4,17 +4,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import pl.bussystem.bussystem.domain.entity.AuthorityEntity;
 import pl.bussystem.bussystem.domain.repository.AccountRepository;
 import pl.bussystem.bussystem.domain.repository.AuthorityRepository;
+import pl.bussystem.bussystem.webui.dto.exception.ExceptionCodes;
 
 import java.util.List;
 
 public class UserTypeRecognitionService {
-  private AccountRepository accountRepository;
+
+  // useless class since we have:
+  //   @PreAuthorize("hasAuthority('read:contacts')")
   private AuthorityRepository authorityRepository;
 
   @Autowired
   public UserTypeRecognitionService(AccountRepository accountRepository, AuthorityRepository authorityRepository) {
-    this.accountRepository = accountRepository;
     this.authorityRepository = authorityRepository;
+  }
+
+
+  public String getUserType(String username) throws Exception {
+    String type;
+
+    // it would be better to have user account types as constants
+    if (isAdmin(username)) {
+      type = "Admin";
+    } else if (isBOK(username)) {
+      type = "BOK";
+    } else if (isDriver(username)) {
+      type = "Driver";
+    } else if (isClient(username)) {
+      type = "Client";
+    } else {
+      throw new Exception("user does not have type!");
+    }
+
+    return type;
   }
 
   public Boolean isAdmin(String username) {
@@ -25,11 +47,11 @@ public class UserTypeRecognitionService {
     return doesHaveAuthority(username, "LogAsDriver");
   }
 
-  public Boolean isBOK(String username){
+  public Boolean isBOK(String username) {
     return doesHaveAuthority(username, "LogAsBOK");
   }
 
-  public Boolean isClient(String username){
+  public Boolean isClient(String username) {
     return doesHaveAuthority(username, "LogAsClient");
   }
 
