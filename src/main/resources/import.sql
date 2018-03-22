ddc2ba4729e9c9ee5fcddd4644490992a8508819
Grzegorz Chilczuk <chilczukg@gmail.com>
2018-03-22 23:15:44 +0100

    repair first insert

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.accounts(active, email, name, password, phone, photo, surname, username) VALUES  (TRUE, 'admin@admin.pl', 'Jan', '$2a$04$epeg1B52UFs0iBLleXk9y.O2R93KRjRA4XljnE0zIes2kVnpOAXSa', '123123123', '', 'Kowalski', 'admin');
-INSERT INTO public.authorities(authority, account) VALUES ('ROLE_BOK', 1);
+INSERT INTO public.authorities(authority, account) VALUES ('ROLE_BOK', (SELECT id FROM accounts WHERE username='admin'));
 
 INSERT INTO public.news(body, date_time, title, author) VALUES ('cialo artykulu', '2015-09-01T16:34:02', 'tytul', 1);
 INSERT INTO public.news(body, date_time, title, author) VALUES ('cialo artykulu', '2015-08-01T16:34:02', 'tytul', 1);
