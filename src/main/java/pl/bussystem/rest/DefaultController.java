3e9d409de2d3da492c6749ea042960958346f295
Paweł P <panczo12d@gmail.com>
2018-04-11 13:28:10 +0200

    removed unused imports

--- src/main/java/pl/bussystem/rest/DefaultController.java
+++ src/main/java/pl/bussystem/rest/DefaultController.java
@@ -1,9 +1,7 @@
 package pl.bussystem.rest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 class DefaultController {
