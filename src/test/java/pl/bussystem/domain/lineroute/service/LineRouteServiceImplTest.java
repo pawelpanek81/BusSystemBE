57c5a65b8b48bed10003e1e692af0db8b96098ec
Paweł P <panczo12d@gmail.com>
2018-04-12 21:06:56 +0200

    fix and improve line route unit test

--- src/test/java/pl/bussystem/domain/lineroute/service/LineRouteServiceImplTest.java
+++ src/test/java/pl/bussystem/domain/lineroute/service/LineRouteServiceImplTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import pl.bussystem.domain.lineinfo.busline.persistence.entity.BusLineEntity;
+import pl.bussystem.domain.lineinfo.busline.service.BusLineService;
 import pl.bussystem.domain.lineinfo.lineroute.persistence.entity.LineRouteEntity;
 import pl.bussystem.domain.lineinfo.lineroute.persistence.repository.LineRouteRepository;
 import pl.bussystem.domain.lineinfo.lineroute.service.LineRouteService;
@@ -26,43 +27,38 @@ public class LineRouteServiceImplTest {
 
   @Mock
   private LineRouteRepository lineRouteRepositoryMock;
+  @Mock
+  private BusLineService busLineServiceMock;
 
   @Before
   public void setUp() {
-    lineRouteService = new LineRouteServiceImpl(lineRouteRepositoryMock, null);
+    lineRouteService = new LineRouteServiceImpl(lineRouteRepositoryMock, busLineServiceMock);
   }
 
   @Test
   public void readByBusLineIdShouldReturnLineRoutesByBusLineId() {
     //given
-    LineRouteEntity lineRoute1 = new LineRouteEntity(
-        1,
-        new BusLineEntity(1, "bus line1", null, null, null),
-        null, null, null
-    );
-    LineRouteEntity lineRoute2 = new LineRouteEntity(
-        2,
-        new BusLineEntity(1, "bus line2", null, null, null),
-        null, null, null
-    );
-    LineRouteEntity lineRoute3 = new LineRouteEntity(
-        3,
-        new BusLineEntity(2, "bus line3", null, null, null),
-        null, null, null
-    );
+    BusLineEntity bus_line1 = new BusLineEntity(1, "bus line1", null, null, null);
+    BusLineEntity bus_line2 = new BusLineEntity(1, "bus line2", null, null, null);
+    BusLineEntity bus_line3 = new BusLineEntity(2, "bus line3", null, null, null);
+
+    LineRouteEntity lineRoute1 = new LineRouteEntity(1, bus_line1, null, null, null);
+    LineRouteEntity lineRoute2 = new LineRouteEntity(2, bus_line2, null, null, null);
+    LineRouteEntity lineRoute3 = new LineRouteEntity(3, bus_line3, null, null, null);
     List<LineRouteEntity> lineRoutes = Arrays.asList(lineRoute1, lineRoute2, lineRoute3);
 
     when(lineRouteRepositoryMock.findAll()).thenReturn(lineRoutes);
+    when(busLineServiceMock.notExistsById(1)).thenReturn(false);
 
     List<LineRouteEntity> expected = new ArrayList<>(Arrays.asList(lineRoute1, lineRoute2));
 
     //when
     List<LineRouteEntity> actual = lineRouteService.readByBusLineId(1);
     verify(lineRouteRepositoryMock, times(1)).findAll();
+    verify(busLineServiceMock, times(1)).notExistsById(1);
 
     //then
-    assertEquals("Lists have different size", expected.size(), actual.size());
     assertThat(actual, is(expected));
-    verifyNoMoreInteractions(lineRouteRepositoryMock);
+    verifyNoMoreInteractions(lineRouteRepositoryMock, busLineServiceMock);
   }
 }
